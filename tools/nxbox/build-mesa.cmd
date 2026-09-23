eebcec1f8bdb0d3986f08e3ad7603cfbf1718142
Miguel <miguel@nspx.dev>
2026-09-23 19:27:36 -0300

    Use runnable host compiler checks before generating UWP Mesa projects [skip ci]

--- tools/nxbox/build-mesa.cmd
+++ tools/nxbox/build-mesa.cmd
@@ -2,10 +2,9 @@
 setlocal
 for /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -property installationPath`) do set "NXBOX_VSROOT=%%i"
 if not defined NXBOX_VSROOT exit /b 1
-call "%NXBOX_VSROOT%\VC\Auxiliary\Build\vcvarsall.bat" x64 uwp
+call "%NXBOX_VSROOT%\VC\Auxiliary\Build\vcvarsall.bat" x64
 if errorlevel 1 exit /b 1
 set "NXBOX_ROOT=%CD%"
-set "LDFLAGS=WindowsApp.lib"
 set "PATH=C:\ProgramData\chocolatey\bin;C:\ProgramData\chocolatey\lib\winflexbison3\tools;%PATH%"
 cd .cache\mesa
 python ..\meson\meson.py setup build-nxbox --backend=vs2022 --uwp --buildtype=release --prefix="%NXBOX_ROOT%\mesa-install" -Dcpp_std=vc++17 -Dcpp_args="['/D_XBOX_UWP']" -Dc_args="['/D_XBOX_UWP']" -Db_pch=false -Dc_winlibs=windowsapp -Dcpp_winlibs=windowsapp -Dgallium-drivers=d3d12 -Dvulkan-drivers=[] -Dllvm=disabled -Ddraw-use-llvm=false -Dgallium-d3d12-video=disabled -Degl=disabled -Dgles1=disabled -Dgles2=disabled -Dbuild-tests=false
